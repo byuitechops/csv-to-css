9f3f945137ed76680f6054ad9ccca71242ddf962
Dowdle <thedowdle@byui.edu>
2018-06-14 16:58:55 -0600

    Verify course code. Broke something. Errors aren't reporting

--- csv_to_css.js
+++ csv_to_css.js
@@ -59,7 +59,20 @@ function urlToCsv(url) {
  * @param {string} csvString 
  */
 function csvToObj(csvString) {
-    var csvObj = dsv.csvParse(csvString);
+    var csvObj = dsv.csvParse(csvString, (row) => {
+        // Trim all values
+        for (let key in row) {
+            row[key] = row[key].trim();
+        }
+
+        // Remove all spaces and lowercase all letters in course code
+        if (row.courseCode) {
+            row.courseCode = row.courseCode
+                .replace(/\s/g, '')
+                .toLowerCase();
+        }
+        return row;
+    });
     return csvObj;
 }
 
@@ -71,23 +84,75 @@ function objToCSS(csvObj) {
     var metaDataTags = csvObj.columns.filter(column => column[0] === '@');
     var cssTags = csvObj.columns.filter(column => column.substr(0, 2) === '--');
 
-    function toKeyVals(row, arrayIn) {
-        return arrayIn.reduce((acc, tag) => {
-            if (row[tag] !== '') {
-                acc += `\n${tag}: ${row[tag]};`;
+
+    function makeValidateError(message, rowIndex, data) {
+        return {
+            message,
+            rowIndex,
+            data
+        };
+    }
+
+    function verifyCourseCode(row, rowIndex) {
+        let error = null;
+
+        if (row.courseCode) {
+            let isValidCourseCode = /^[a-z]{1,5}\d{3}(?:-[a-z]+)?$/.test(row.courseCode);
+            if (!isValidCourseCode) {
+                error = makeValidateError('Invalid course code', rowIndex);
+            }
+        } else {
+            error = makeValidateError('There is no course code', rowIndex);
+        }
+        return error; //what it returns
+    }
+
+    function verifyRow(row, rowIndex) {
+        let errors = [
+            verifyCourseCode(row, rowIndex)
+        ];
+
+        return errors.filter(e => e !== null);
+    }
+
+    function toKeyVals(row, keysWeWant) {
+        return keysWeWant.reduce((acc, key) => {
+            if (row[key] !== '') {
+                acc += `\n${key.replace(/^@/, '')}: ${row[key]};`;
             }
             return acc;
         }, '');
     }
 
-    var cssStrings = csvObj.map(row => {
-        var metaData = '/*' + toKeyVals(row, metaDataTags).replace(/@/g, '') + '\n*/';
-        var cssData = `.${row.courseCode} {` + toKeyVals(row, cssTags) + '\n}';
-        if (cssData !== '. {\n}') return `${metaData}\n${cssData}\n${row.customCSS ? row.customCSS + '\n' : ''}`;
-        return null;
+
+    let cssObjOut = csvObj.reduce((acc, row, rowIndex) => {
+
+        // Verify that the row has the correct parts/is valid
+        let errors = verifyRow(row, rowIndex);
+        // Check for errors 
+        if (errors.length > 0) {
+            //add errors to the acc.err
+            acc.err.push(errors);
+        } else {
+            // create the css string 
+            var metaData = '/*' + toKeyVals(row, metaDataTags) + '\n*/';
+            var cssData = `.${row.courseCode} {` + toKeyVals(row, cssTags) + '\n}';
+            // Skip the blank css rows
+            if (cssData !== '. {\n}') {
+                // add it to acc.css
+                acc.css += `${metaData}\n${cssData}\n${row.customCSS ? row.customCSS + '\n' : ''}\n`;
+            }
+        }
+
+        return acc;
+    }, {
+        css: '',
+        err: []
     });
-    return cssStrings.filter(str => str !== null).join('\n');
+
+    return cssObjOut;
 }
+
 
 /**
  * change the js object to correctly formatted css.
@@ -96,6 +161,7 @@ function objToCSS(csvObj) {
  */
 function validateCss(cssString, path) {
     let errors = reporter(validate.validateString(cssString, path));
+    console.log(errors);
     let parsedErrors;
     try {
         parsedErrors = JSON.parse(errors);
@@ -117,6 +183,7 @@ function minifyCSS(cssString, fileName) {
 
     // validate the minified file
     let errors = validateCss(output.styles, fileName);
+    console.log(errors);
 
     // If there are no errors, it will return the minified file
     if (errors.length === 0) {
@@ -127,7 +194,7 @@ function minifyCSS(cssString, fileName) {
 
         // If there are validation errors it will return the errors.
     } else {
-        console.log(chalk.redBright(`There was an error minifying ${fileName}`));
+        console.error(chalk.redBright(`There was an error minifying ${fileName}`));
         return {
             'output': errors,
             'valid': false
@@ -157,11 +224,11 @@ function writeFile(path, fileGuts) {
  */
 function cssFileError(errorFile, errors) {
     try {
-    writeFile(errorFile, JSON.stringify(errors, null, 4));
-    } catch(err) {
+        writeFile(errorFile, JSON.stringify(errors, null, 4));
+    } catch (err) {
         errorHandling(err);
     }
-    console.log(chalk.magenta(`Check ${errorFile} for errors`));
+    console.log(chalk.redBright(`Check ${errorFile} for errors`));
 }
 
 /**
@@ -184,7 +251,8 @@ async function main() {
             let csvString = await urlToCsv(settings[i].url);
             let cssObj = csvToObj(csvString);
             let cssString = objToCSS(cssObj);
-            let errors = validateCss(cssString, fileName);
+            let errors = validateCss(cssString.css, fileName);
+            // if (cssString.err.length > 0) errors = cssString.err;
             let errorFile = `${fileName}_errors.json`;
             let newHash;
             let minify = {
@@ -195,13 +263,13 @@ async function main() {
             if (errors.length !== 0) {
                 cssFileError(errorFile, errors);
             } else {
-                minify = minifyCSS(cssString, fileName);
+                minify = minifyCSS(cssString.css, fileName);
                 if (minify.valid === false) {
                     cssFileError(errorFile, minify.output);
                 } else {
                     newHash = md5(minify.output);
                     if (newHash !== settings[i].hash) {
-                        writeFile(`${fileName}.css`, cssString);
+                        writeFile(`${fileName}.css`, cssString.css);
                         writeFile(`${fileName}_mini.css`, minify.output);
                         settings[i].hash = newHash;
                     }
@@ -213,11 +281,9 @@ async function main() {
     }
     try {
         formattedSettings = JSON.stringify(settings, null, 4);
-    } catch(err) {
+    } catch (err) {
         errorHandling(err);
     }
-    console.log(settingsHash);
-    console.log(md5(formattedSettings));
     if (settingsHash !== md5(formattedSettings)) {
         try {
             fs.writeFileSync(`${__dirname}/settings.json`, formattedSettings);
@@ -226,7 +292,7 @@ async function main() {
         }
         console.log(chalk.green('Settings updated'));
     } else {
-        console.log(chalk.green('Nothing changed'));
+        console.log(chalk.green('No files changed'));
     }
 }
 
