3068ef1dea1c1af1e184ab3e053d9af9131ddbad
Dowdle <thedowdle@byui.edu>
2018-06-20 17:07:35 -0600

    Move objToCss to seperate file

--- csv_to_css.js
+++ csv_to_css.js
@@ -14,7 +14,9 @@ const md5 = require('md5');
 const path = require('path');
 const date = new Date();
 const dateUpdated = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
+const objToCss = require('./objToCss');
 let settingsHash;
+
 
 // read the settings from wherever they are
 
@@ -67,7 +69,8 @@ function csvToObj(csvString) {
         for (let key in row) {
             row[key] = row[key].trim();
             if (!(key.includes('@') || key.includes('--')) && row[key] !== '') {
-                requiredValues.push(`${key}: ${row[key]}`);
+                requiredValues.push(`{${key}: ${row[key]}}`);
+                delete row[key];
             }
         }
 
@@ -78,129 +81,14 @@ function csvToObj(csvString) {
                 .toLowerCase();
         }
 
-
-
         return row;
     });
-
-    console.log(requiredValues);
-    return csvObj;
+    console.log(csvObj)
+    return {
+        csvObj,
+        requiredValues
+    };
 }
-
-/**
- * change the js object to correctly formatted css.
- * @param {object} csvObj 
- */
-function objToCSS(csvObj) {
-    var metaDataTags = csvObj.columns.filter(column => column[0] === '@');
-    var cssTags = csvObj.columns.filter(column => column.substr(0, 2) === '--');
-    let department;
-
-    function makeValidateError(message, rowIndex, data) {
-        return {
-            message,
-            rowIndex,
-            data
-        };
-    }
-
-    function verifyCourseCode(row, rowIndex) {
-        let error = null;
-
-        if (row.courseCode) {
-            let courseCode = row.courseCode;
-            let isValidCourseCode = /^[a-z]{1,5}\d{3}(?:-[a-z]+)?$/.test(courseCode);
-            if (!isValidCourseCode) {
-                error = makeValidateError(`${courseCode}: Invalid course code`, rowIndex, row);
-                console.error(`${courseCode} is not a valid Course Code`);
-            }
-        } else {
-            error = makeValidateError('There is no course code', rowIndex, row);
-        }
-        return error; //what it returns
-    }
-
-    function verifyRow(row, rowIndex) {
-        let errors = [
-            verifyCourseCode(row, rowIndex),
-        ];
-
-        department = row.department ? row.department : null;
-
-        return errors.filter(e => e !== null);
-    }
-
-    function toKeyVals(row, keysWeWant) {
-        return keysWeWant.reduce((acc, key) => {
-            if (row[key] !== '') {
-                acc += `\n${key.replace(/^@/, '')}: ${row[key]};`;
-            }
-            return acc;
-        }, '');
-    }
-
-    function createCssStringAndObject(department, row, err) {
-        let returnedObj = {
-            department,
-            css: '',
-            err: []
-        };
-        if (err.length > 0) {
-            //add errors to the acc.err
-            returnedObj.err = err;
-        }
-
-        var metaData = '/*' + toKeyVals(row, metaDataTags) + '\n*/';
-        var cssData = `.${row.courseCode} {` + toKeyVals(row, cssTags) + '\n}';
-
-        if (cssData !== '. {\n}') {
-            returnedObj.css = `${metaData}\n${cssData}\n${row.customCSS ? row.customCSS + '\n' : ''}\n`;
-        }
-        return returnedObj;
-    }
-
-    let cssObjOut = csvObj.reduce((acc, row, rowIndex) => {
-
-        /**
-         * Potential change to handle departments:
-         *  A different function to create the css string.
-         *  inside that function have the course code declared and added to the object.
-         *  Return the object, and add that to the acc array.
-         */
-
-
-
-        // Verify that the row has the correct parts/is valid
-        let errors = verifyRow(row, rowIndex);
-
-
-        let things = createCssStringAndObject(department, row, errors);
-        // console.log(things);
-        acc.push(things);
-
-
-        // Check for errors 
-        // if (errors.length > 0) {
-        //     //add errors to the acc.err
-        //     acc.err.push(errors);
-        // }
-        // // else {
-        // // create the css string 
-        // var metaData = '/*' + toKeyVals(row, metaDataTags) + '\n*/';
-        // var cssData = `.${row.courseCode} {` + toKeyVals(row, cssTags) + '\n}';
-        // // Skip the blank css rows
-        // if (cssData !== '. {\n}') {
-        //     // add it to acc.css
-        //     acc.css += `${metaData}\n${cssData}\n${row.customCSS ? row.customCSS + '\n' : ''}\n`;
-        //     // }
-        // }
-
-        return acc;
-    }, []);
-
-    return cssObjOut;
-}
-
 
 /**
  * change the js object to correctly formatted css.
@@ -299,8 +187,7 @@ async function main() {
             let fileName = settings[i].fileName;
             let csvString = await urlToCsv(settings[i].url);
             let cssObj = csvToObj(csvString);
-            let cssFinalObjects = objToCSS(cssObj); // An array of objects. 1 object per row.
-            // console.log(`This is the Array/Object of power ${cssFinalObjects}`);
+            let cssFinalObjects = objToCss(cssObj.csvObj); // An array of objects. 1 object per row.
 
             let errors = validateCss(cssFinalObjects[1], fileName);
             let errorFile = `${fileName}_errors_${dateUpdated}.json`;
